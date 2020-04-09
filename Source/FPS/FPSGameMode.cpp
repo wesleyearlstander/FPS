42d7969d48f8b7e7e99ce90aa2b36e9d391a9004
wesleyearlstander <wesleyearlstander@gmail.com>
2020-04-09 16:20:24 +0200

    Initial Commit

---
+++ Source/FPS/FPSGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "FPSGameMode.h"
+#include "FPSCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AFPSGameMode::AFPSGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
